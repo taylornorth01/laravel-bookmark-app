c062a131669f492af9de31eacbce1aa7b3d80c4e
taylornorth01 <taylornorth13@hotmail.com>
2021-03-25 01:49:21 +0000

    Bookmark factory select random category

--- database/factories/BookmarkFactory.php
+++ database/factories/BookmarkFactory.php
@@ -3,11 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\Bookmark;
+use App\Category;
 use Faker\Generator as Faker;
 
 $factory->define(Bookmark::class, function (Faker $faker) {
     return [
         'link_title' => $faker->sentence(3),
+        'category_id' => Category::inRandomOrder()
+                                    ->take(1)
+                                    ->get('id')[0],
         'url' => $faker->url,
         'publication_date' => $faker->dateTimeThisYear('-2 months'),
         'date_last_accessed' => $faker->dateTimeThisMonth('-12 days')
