62cc0ad37669f135ccda43601cbb48f6b4355681
taylornorth01 <taylornorth13@hotmail.com>
2021-04-01 14:56:44 +0100

    Refactor clean up bookmark controller

--- app/Http/Controllers/BookmarkController.php
+++ app/Http/Controllers/BookmarkController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Bookmark;
 use App\Category;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\Redirect;
 
@@ -13,57 +12,32 @@ use App\Http\Requests\BookmarkRequest;
 
 class BookmarkController extends Controller {
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index() {
         return View::make('bookmark.index', [
             'bookmarks' => Bookmark::latest()->paginate(7)
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function create() {
         return View::make('bookmark.create', [
             'categories' => Category::all()
         ]);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+
     public function store(BookmarkRequest $request) {
         Bookmark::create($request->validated());
         return Redirect::route('bookmarks.index');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Bookmark  $bookmark
-     * @return \Illuminate\Http\Response
-     */
+
     public function show(Bookmark $bookmark) {
-        return View::make('bookmark.show', [
-            'bookmark' => $bookmark
-        ]);
+        return View::make('bookmark.show', compact('bookmark'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Bookmark  $bookmark
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit(Bookmark $bookmark) {
         return View::make('bookmark.edit', [
             'bookmark' => $bookmark,
@@ -71,24 +45,13 @@ class BookmarkController extends Controller {
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Bookmark  $bookmark
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(BookmarkRequest $request, Bookmark $bookmark) {
         $bookmark->update($request->validated());
         return Redirect::route('bookmarks.show', $bookmark);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Bookmark  $bookmark
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy(Bookmark $bookmark) {
         $bookmark->delete();
         return Redirect::route('bookmarks.index');
