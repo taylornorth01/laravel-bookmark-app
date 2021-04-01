8831eaccd1238ad5c5a6f1613c16da58ac3a2f87
taylornorth01 <taylornorth13@hotmail.com>
2021-04-01 18:28:45 +0100

    Created category validation and fillable fields

--- app/Category.php
+++ app/Category.php
@@ -4,8 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
-{
+class Category extends Model {
+
+    protected $fillable = ['category_name'];
+
     public function bookmarks() {
         return $this->hasMany(Bookmark::class);
     }
