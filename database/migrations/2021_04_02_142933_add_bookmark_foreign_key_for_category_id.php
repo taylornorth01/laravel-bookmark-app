2944dae35480ece3df368bb09b3b6c0b3cce8965
taylornorth01 <taylornorth13@hotmail.com>
2021-04-02 15:44:55 +0100

    New migration for foreign key delete set null

---
+++ database/migrations/2021_04_02_142933_add_bookmark_foreign_key_for_category_id.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddBookmarkForeignKeyForCategoryId extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('bookmarks', function (Blueprint $table) {
+            $table->foreign('category_id')
+                ->references('id')->on('categories')
+                ->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('bookmarks', function (Blueprint $table) {
+            $table->dropForeign('bookmarks_category_id_foreign');
+        });
+    }
+}
